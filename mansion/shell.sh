b4d1b1c3cee685e921a83e11e5306954684a337b
VasishtaChow <60466545+VasishtaChow@users.noreply.github.com>
2020-12-17 11:26:09 +0530

    Update shell.sh

--- mansion/shell.sh
+++ mansion/shell.sh
@@ -1,5 +1,5 @@
 mkdir docker
 cp Dockerfile ./docker/Dockerfile
 cd docker
-docker build -f Dockerfile -t test:0.1 .
-docker push test:0.1
+sudo docker build -f Dockerfile -t test:0.1 .
+sudo docker push test:0.1
